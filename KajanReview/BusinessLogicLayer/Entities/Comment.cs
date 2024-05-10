af2c5a662779b337b01930fa17cd06546e020d71
Rastislav Kajan <515059@student.fontys.nl>
2024-05-10 17:14:16 +0200

    Changed entities to implement from IUserPost Made Body nullable for Review

--- KajanReview/BusinessLogicLayer/Entities/Comment.cs
+++ KajanReview/BusinessLogicLayer/Entities/Comment.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessLogicLayer.Interfaces;
 
 namespace BusinessLogicLayer.Entities
 {
-    public class Comment
+    public class Comment : IUserPost
     {
+
+        public int ID { get; set; }
+
+        public User Poster { get; set; }
+
+        public string Body { get; set; }
+
+        public int UpvoteCount { get; set; }
+
+        public int DownvoteCount { get; set; }
+
+        public DateTime PostDate { get; set; }
         public Review SourceReview { get; set; }
     }
 }
--- KajanReview/BusinessLogicLayer/Entities/Review.cs
+++ KajanReview/BusinessLogicLayer/Entities/Review.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessLogicLayer.Interfaces;
 
 namespace BusinessLogicLayer.Entities
 {
-    public class Review
+    public class Review : IUserPost
     {
+        public int ID { get; set; }
+        public User Poster { get; set; }
+        public string? Body { get; set; }
+        // Make 0 if Body is null
+        public int UpvoteCount { get; set; }
+        //Makae 0 if Body is null
+        public int DownvoteCount { get; set; }
+        public DateTime PostDate { get; set; }
         public int BookRating { get; set; }
         public List<Comment>? Comments { get; set; }
     }
