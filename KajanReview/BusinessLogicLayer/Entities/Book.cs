4451b9a10b939795ebc792b9e01841faa2c7cb13
Rastislav Kajan <515059@student.fontys.nl>
2024-05-10 15:16:21 +0200

    Made the CoverFilePath property nullable

--- KajanReview/BusinessLogicLayer/Entities/Book.cs
+++ KajanReview/BusinessLogicLayer/Entities/Book.cs
@@ -12,7 +12,7 @@ namespace BusinessLogicLayer.Entities
     public class Book
     {
         public int ID { get; set; }
-        public string CoverFilePath { get; set; }
+        public string? CoverFilePath { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public int PageCount { get; set; }
