141c52659c753aa52f08ff11cb270f9f3ef74ef5
Rastislav Kajan <515059@student.fontys.nl>
2024-05-10 17:13:44 +0200

    Previously UserPost Made properties private Made Setters private

---
+++ KajanReview/BusinessLogicLayer/Interfaces/IUserPost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogicLayer.Entities;
+
+namespace BusinessLogicLayer.Interfaces
+{
+    public interface IUserPost
+    {
+        int ID { get; }
+        User Poster { get; }
+        string Body { get; }
+        int UpvoteCount { get; }
+        int DownvoteCount { get; }
+        DateTime PostDate { get; }
+    }
+}
