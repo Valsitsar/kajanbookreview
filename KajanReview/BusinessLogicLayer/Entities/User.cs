1eb7dc3f8c13cabf09459be88d026c9638d06a66
Rastislav Kajan <515059@student.fontys.nl>
2024-06-02 23:18:25 +0200

    `Refactor User class and update SignIn functionality`
     Renamed `ProfilePicturePath` to `ProfilePictureFilePath` in `User` class. Updated `asp-page` attribute in `_Layout.cshtml` for sign-in link. Removed content of `SignIn.cshtml` and `SignIn.cshtml.cs`, including the `SignInModel` class and its methods.

--- KajanReview/BusinessLogicLayer/Entities/User.cs
+++ KajanReview/BusinessLogicLayer/Entities/User.cs
@@ -6,7 +6,7 @@ namespace BusinessLogicLayer.Entities
     public class User
     {
         public int ID { get; set; }
-        public string? ProfilePicturePath { get; set; }
+        public string? ProfilePictureFilePath { get; set; }
 
         [Required]
         public string FirstName { get; set; } = string.Empty;
