983a4186af75e206723c6e425fc149060029bf3c
Rastislav Kajan <515059@student.fontys.nl>
2024-06-02 23:00:42 +0200

    Subject: Added validation to User class properties
    Body:
    Removed initialization of `ProfilePicturePath` property, but it remains nullable.
    Added `Required` attribute to `FirstName`, `LastName`, `Username`, `Password`, and `Email` properties to ensure they cannot be null or empty.
    Added `DataType.Password` to `Password` property for special UI treatment.
    Added `EmailAddress` attribute to `Email` property to enforce valid email address format.

--- KajanReview/BusinessLogicLayer/Entities/User.cs
+++ KajanReview/BusinessLogicLayer/Entities/User.cs
@@ -1,16 +1,29 @@
 ﻿using BusinessLogicLayer.Interfaces;
+using System.ComponentModel.DataAnnotations;
 
 namespace BusinessLogicLayer.Entities
 {
     public class User
     {
         public int ID { get; set; }
-        public string? ProfilePicturePath { get; set; } = string.Empty; // TODO: Add to DB + Maybe don't need to init
+        public string? ProfilePicturePath { get; set; }
+
+        [Required]
         public string FirstName { get; set; } = string.Empty;
         public string? MiddleNames { get; set; }
+
+        [Required]
         public string LastName { get; set; } = string.Empty;
+
+        [Required]
         public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
         public List<Bookshelf> Bookshelves { get; set; } = new List<Bookshelf>();
