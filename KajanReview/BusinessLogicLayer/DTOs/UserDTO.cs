60abd3f1f1c5b7c433f6529b444323b7c369058e
Rastislav Kajan <515059@student.fontys.nl>
2024-06-07 23:01:21 +0200

    Added UserDTO.cs and updated User.cs
    Added a new file UserDTO.cs in the LogicLayer, defining a new class UserDTO with several properties.
    Made several changes to the User class in User.cs file, including removal of certain attributes and changing the PhoneNumber property to a nullable string.

--- KajanReview/BusinessLogicLayer/DTOs/UserDTO.cs
+++ KajanReview/BusinessLogicLayer/DTOs/UserDTO.cs
@@ -1,21 +1,34 @@
-﻿using BusinessLogicLayer.Interfaces;
+﻿using BusinessLogicLayer.Entities;
+using BusinessLogicLayer.Interfaces;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace BusinessLogicLayer.Entities
+namespace BusinessLogicLayer.DTOs
 {
-    public class User
+    public class UserDTO
     {
         public int ID { get; set; }
         public string? ProfilePictureFilePath { get; set; } = "~/img/default-profile-picture.png";
+
         public string? FirstName { get; set; }
         public string? MiddleNames { get; set; }
+
         public string? LastName { get; set; }
+
+        [Required(ErrorMessage = "CurrentUsername is required.")]
         public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
         public string Email { get; set; } = string.Empty;
         public string? PhoneNumber { get; set; }
-        public List<Bookshelf> Bookshelves { get; set; } = new List<Bookshelf>();
-        public List<Role> Roles { get; set; } = new List<Role>();
-        public List<IUserPost> Posts { get; set; } = new List<IUserPost>();
     }
 }
