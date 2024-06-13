979bc6e55bee54a8cd067f12972dd3f7d3c4f122
Rastislav Kajan <515059@student.fontys.nl>
2024-06-13 14:02:19 +0200

    Add pagination and dynamic data loading (requires testing)
    - Removed hardcoded static book list from Index.cshtml.cs.
    - Eliminated example data and reviews, shifting to real dynamic data sources.
    - Implemented pagination controls in Index.cshtml for improved navigation.
    - Added pagination properties to IndexModel for dynamic data handling.
    - Modified OnGet to load data asynchronously based on page number.
    - Refactored IndexModel constructor for dependency injection with IBookManager.

--- KajanReview/Web_App/Pages/Index.cshtml.cs
+++ KajanReview/Web_App/Pages/Index.cshtml.cs
@@ -7,125 +7,32 @@ namespace Web_App.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        // Properties for pagination
         public List<Book> Books { get; set; }
+        public int TotalBooks { get; set; }
+        public int PageSize { get; set; } = 10; // Default page size
+        public int CurrentPage { get; set; } = 1;
+        public int TotalPages { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+
+        public IndexModel(IBookManager bookManager)
         {
-            _logger = logger;
+            _bookManager = bookManager;
         }
 
-        public void OnGet()
+        public async Task OnGet(int? pageNumber)
         {
-            Books = new List<Book>
-            {
-                new Book
-                {
-                    CoverFilePath = "./img/Book_cover_unavailable.png",
-                    Title = "Example book",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt leo imperdiet ante venenatis, et tempus odio eleifend.",
-                    PageCount = 1000,
-                    ISBN = "1234567654321",
-                    Format = new BookFormat
-                    {
-                        Name = "Hardcover"
-                    },
-                    Publisher = "Example publisher",
-                    PubDate = DateTime.Today,
-                    Language = "English",
-                    Genres = new List<Genre>
-                    {
-                        new Genre { Name = "Fantasy" },
-                        new Genre { Name = "Romance" }
-                    },
-                    Authors = new List<User>
-                    {
-                        new User { FirstName = "Example", LastName = "Author" }
-                    }
-                },
-                new Book
-                {
-                    CoverFilePath = "./img/Book_cover_unavailable.png",
-                    Title = "Example book 2",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt leo imperdiet ante venenatis, et tempus odio eleifend. Vivamus a magna arcu. In nisl ipsum, cursus sit amet posuere eu, bibendum eget mauris. Pellentesque semper semper orci, ut vehicula enim ultrices in. Duis tempor vulputate ante vel viverra. Sed fringilla dolor eget neque interdum, laoreet facilisis massa condimentum. Etiam ultrices sem non nunc gravida aliquet. Nunc eu ligula ut libero pulvinar porttitor a vitae nunc. Maecenas et nulla sem. Fusce ac risus ac justo dictum luctus id eu augue.",
-                    PageCount = 500,
-                    ISBN = "1234567654321",
-                    Format = new BookFormat
-                    {
-                        Name = "Paperback"
-                    },
-                    Publisher = "Example publisher 2",
-                    PubDate = DateTime.Today,
-                    Language = "French",
-                    Genres = new List<Genre>
-                    {
-                        new Genre { Name = "Mystery" },
-                        new Genre { Name = "Thriller" },
-                        new Genre { Name = "Dark Fantasy" }
-                    },
-                    Authors = new List<User>
-                    {
-                        new User { FirstName = "FirstName", LastName = "Surname" }
-                    }
-                },
-                new Book()
-                {
-                    CoverFilePath = "./img/Book_cover_unavailable.png",
-                    Title = "Example book",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt leo imperdiet ante venenatis, et tempus odio eleifend. Vivamus a magna arcu. In nisl ipsum, cursus sit amet posuere eu, bibendum eget mauris. Pellentesque semper semper orci, ut vehicula enim ultrices in. Duis tempor vulputate ante vel viverra. Sed fringilla dolor eget neque interdum, laoreet facilisis massa condimentum. Etiam ultrices sem non nunc gravida aliquet. Nunc eu ligula ut libero pulvinar porttitor a vitae nunc. Maecenas et nulla sem. Fusce ac risus ac justo dictum luctus id eu augue.",
-                    PageCount = 1000,
-                    ISBN = "1234567654321",
-                    Format = new BookFormat() { Name = "Hardcover" },
-                    Publisher = "Example publisher",
-                    PubDate = DateTime.Today,
-                    Language = "English",
-                    Genres = new List<Genre>
-                    {
-                        new Genre() { Name = "Fantasy" }, new Genre() { Name = "Romance" }
-                    },
-                    Authors = new List<User> { new User() { FirstName = "Example", LastName = "Author" } },
-                    Reviews = new List<Review>
-                    {
-                        new Review()
-                        {
-                            Poster = new User() { Username = "ReviewPoster1" },
-                            Title = "I loved it!",
-                            Body = "This book is amazing!",
-                            BookRating = 5,
-                            PostDate = DateTime.Today.AddDays(-4),
-                            UpvoteCount = 69,
-                        },
-                        new Review()
-                        {
-                            Poster = new User() { Username = "ReviewPoster2" },
-                            Title = "A mediocre experience.",
-                            Body = "This book is okay.",
-                            BookRating = 3,
-                            PostDate = DateTime.Today.AddDays(-3),
-                            UpvoteCount = 420,
-                        },
-                        new Review()
-                        {
-                            Poster = new User() { Username = "ReviewPoster3" },
-                            Title = "Do not recommend to anyone. Stay away!",
-                            Body = "This book is terrible.",
-                            BookRating = 1,
-                            PostDate = DateTime.Today.AddDays(-2),
-                            DownvoteCount = 666,
-                        },
-                        new Review()
-                        {
-                            Poster = new User() { Username = "ReviewPoster4" },
-                            Title = "This book changed my life - here's how.",
-                            Body = "This book is the best book ever!",
-                            BookRating = 5,
-                            PostDate = DateTime.Today.AddDays(-1)
-                        },
-                        new Review() { BookRating = 2 }, 
-                        new Review() { BookRating = 4 },
-                        new Review() { BookRating = 5 },
-                    }
-                }
-            };
+            // Take the page number from the query string or set to 1
+            CurrentPage = pageNumber ?? 1;
+
+            // Get the total number of books
+            TotalBooks = await _bookManager.GetTotalBooksCountAsync();
+
+            // Calculate the total number of pages
+            TotalPages = (int)Math.Ceiling(TotalBooks / (double)PageSize);
+
+            // Get the paginated list of books
+            Books = await _bookManager.GetBooksByPageAsync(CurrentPage, PageSize);
         }
     }
 }
